96d270d59b6319bdde0fc019724830ec1d0c72fd
tiy-he <tiy_he@foxmail.com>
2018-03-30 11:06:42 +0800

    鼠标点击动画更新

--- web/src/main/webapp/js/xh/mouse-anim.js
+++ web/src/main/webapp/js/xh/mouse-anim.js
@@ -6,6 +6,10 @@
 $(function () {
     var str = ["富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善"];
 
+    var random = function () {
+        return Math.floor(Math.random()*256);
+    };
+
     $(document).on("click","body",function (e) {
         var index = Math.floor(Math.random()*str.length);
         var div = $("<div class='barrage'>"+str[index]+"</div>");
@@ -13,13 +17,15 @@ $(function () {
         var top = e.pageY - $(this).scrollTop();
         div.css({
             "top":+top+"px",
-            "left":e.pageX+"px"
+            "left":e.pageX+"px",
+            "color":"rgb("+random()+","+random()+","+random()+")"
         });
         div.animate({
             "top":top-200+"px",
             "opacity":0.5
-        },1000,function(){
+        },1300,function(){
             div.remove();
         });
     });
+
 });
