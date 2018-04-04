e69ff89b97f907d8387746ac06ad207105bb4739
tiy-he <tiy_he@foxmail.com>
2018-04-04 17:48:38 +0800

    分类管理

--- web/src/main/webapp/web/org/js/category_management.js
+++ web/src/main/webapp/web/org/js/category_management.js
@@ -9,7 +9,7 @@ $(function () {
     /*$("#paging").paging(2,1,function (page) {
         console.log(page);
     });*/
-    $(".del").click(function () {
+    $(document).on("click",".del",function () {
         if(confirm("确定要删除吗")){
             var tr = $(this).parent().parent();
             var id = tr.attr("categoryId");
@@ -19,13 +19,20 @@ $(function () {
         }
     });
 
-    $(".rename").click(function () {
-        var td = $(this).parent().parent().find(".name");
+    $(document).on("click",".rename",function () {
+        var tr = $(this).parent().parent();
+        if(tr.hasClass("on")){
+            return;
+        }
+        tr.addClass("on");
+        var td = tr.find(".name");
         var name = td.text();
         td.attr("name",name);
-        td.html("<input placeholder=\"输入新的分类名\" value=\""+name+"\">");
+        var inp = $("<input placeholder=\"输入新的分类名\" value=\""+name+"\">");
+        td.html(inp);
         td.append("<a href=\"javascript:void(0);\" class=\"save\">保存</a>");
         td.append("<a href=\"javascript:void(0);\" class=\"cancel\">取消</a>");
+        inp.focus();
     });
 
     $(document).on("click",".save",function () {
@@ -39,11 +46,13 @@ $(function () {
 
         //更新ui
         td.html(newName);
+        td.parent().removeClass("on");
     });
 
     $(document).on("click",".cancel",function () {
         var td = $(this).parent();
         td.html(td.attr("name"));
+        td.parent().removeClass("on");
     });
 
     $(".add a").click(function () {
@@ -54,10 +63,12 @@ $(function () {
         }
         var tbody = $("#category_tab").find("tbody");
         var tr = $("<tr></tr>");
-        tr.append("<td>"+val+"</td>");
+        tr.append("<td class='name'>"+val+"</td>");
         tr.append("<td>"+"2018-03-29 17:58:44"+"</td>");
         tr.append("<td><a href=\"javascript:void(0);\" class=\"rename\">重命名</a>" +
             "<a href=\"javascript:void(0);\" class=\"del\">删除</a></td>");
+
+        tr.attr("categoryId",1);
 
         inp.val("");
         tbody.append(tr);
@@ -67,7 +78,7 @@ $(function () {
 });
 
 function showHideAdd() {
-    if($("#category_tab").find("tbody").find("tr").length >= 9){
+    if($("#category_tab").find("tbody").find("tr").length >= 10){
         $(".add").hide();
     }else{
         $(".add").show();
