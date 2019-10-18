3f34701a5e1dda329f5fe03f3e48bcfeddc9329a
xiaohe66 <ty@xiaohe66.com>
2019-10-18 18:08:25 +0800

    异常类规范化

--- xiaohe66-web-portal/src/main/java/com/xiaohe66/web/base/exception/NotPermittedException.java
+++ xiaohe66-web-portal/src/main/java/com/xiaohe66/web/base/exception/NotPermittedException.java
@@ -1,10 +1,12 @@
 package com.xiaohe66.web.base.exception;
 
+import com.xiaohe66.common.ex.XhRuntimeException;
+
 /**
  * @author xiaohe
  * @date 2019.10.13 14:37
  */
-public class NotPermittedException extends GeneralException {
+public class NotPermittedException extends XhRuntimeException {
 
 
     public NotPermittedException() {
