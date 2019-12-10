d6a8ba1092d3c66915d7b228ad7ddf36c8c44266
xiaohe66 <ty@xiaohe66.com>
2019-12-10 18:31:50 +0800

    okHttp集成

---
+++ xiaohe66-web-portal/src/main/java/com/xiaohe66/web/base/base/BaseResponse.java
@@ -0,0 +1,10 @@
+package com.xiaohe66.web.base.base;
+
+import java.io.Serializable;
+
+/**
+ * @author xiaohe
+ * @time 2019.12.10 15:39
+ */
+public interface BaseResponse extends Serializable {
+}
