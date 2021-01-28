c953dcc8de3e0e3a73e4798c8e8fe6b0884ba435
xiaohe66 <ty@xiaohe66.com>
2021-01-28 12:12:47 +0800

    角色管理

--- xiaohe66-web-portal/src/main/java/com/xiaohe66/web/code/security/po/Role.java
+++ xiaohe66-web-portal/src/main/java/com/xiaohe66/web/code/security/po/Role.java
@@ -2,38 +2,19 @@ package com.xiaohe66.web.code.security.po;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.xiaohe66.web.base.base.BasePo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author xiaohe
  * @time 17-10-28 028
  */
+@EqualsAndHashCode(callSuper = true)
 @TableName("xiaohe66_web_security_role")
+@Data
 public class Role extends BasePo {
 
     private String roleName;
     private String roleDesc;
-
-    public String getRoleName() {
-        return roleName;
-    }
-
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
-
-    public String getRoleDesc() {
-        return roleDesc;
-    }
-
-    public void setRoleDesc(String roleDesc) {
-        this.roleDesc = roleDesc;
-    }
-
-    @Override
-    public String toString() {
-        return "{" + "\"roleName\":\"" + roleName + "\""
-                + ", \"roleDesc\":\"" + roleDesc + "\""
-                + ", \"id\":\"" + id + "\""
-                + "}";
-    }
+    private Boolean isDefault;
 }
